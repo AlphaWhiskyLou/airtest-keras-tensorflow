4c86ac2536f4fb6532df263a23dda655b34169c9
Tianyu Lou <3089608641@qq.com>
2021-08-16 11:26:17 +0800

    Finale

--- keras_test.air/keras_test.py
+++ keras_test.air/keras_test.py
@@ -476,82 +476,6 @@ def box_iou (b1, b2):
 
     return iou
 
-"""
-def yolo_loss (args, anchors, num_classes, ignore_thresh=.5, print_loss=False):
-    '''Return yolo_loss tensor
-
-    Parameters
-    ----------
-    yolo_outputs: list of tensor, the output of yolo_body or tiny_yolo_body
-    y_true: list of array, the output of preprocess_true_boxes
-    anchors: array, shape=(N, 2), wh
-    num_classes: integer
-    ignore_thresh: float, the iou threshold whether to ignore object confidence loss
-
-    Returns
-    -------
-    loss: tensor, shape=(1,)
-
-    '''
-    num_layers = len(anchors) // 3  # default setting
-    yolo_outputs = args[:num_layers]
-    y_true = args[num_layers:]
-    anchor_mask = [[6, 7, 8], [3, 4, 5], [0, 1, 2]] if num_layers == 3 else [[3, 4, 5], [1, 2, 3]]
-    input_shape = K.cast(K.shape(yolo_outputs[0])[1:3] * 32, K.dtype(y_true[0]))
-    grid_shapes = [K.cast(K.shape(yolo_outputs[l])[1:3], K.dtype(y_true[0])) for l in range(num_layers)]
-    loss = 0
-    m = K.shape(yolo_outputs[0])[0]  # batch size, tensor
-    mf = K.cast(m, K.dtype(yolo_outputs[0]))
-
-    for l in range(num_layers):
-        object_mask = y_true[l][..., 4:5]
-        true_class_probs = y_true[l][..., 5:]
-
-        grid, raw_pred, pred_xy, pred_wh = yolo_head(yolo_outputs[l],
-                                                     anchors[anchor_mask[l]], num_classes, input_shape, calc_loss=True)
-        pred_box = K.concatenate([pred_xy, pred_wh])
-
-        # Darknet raw box to calculate loss.
-        raw_true_xy = y_true[l][..., :2] * grid_shapes[l][::-1] - grid
-        raw_true_wh = K.log(y_true[l][..., 2:4] / anchors[anchor_mask[l]] * input_shape[::-1])
-        raw_true_wh = K.switch(object_mask, raw_true_wh, K.zeros_like(raw_true_wh))  # avoid log(0)=-inf
-        box_loss_scale = 2 - y_true[l][..., 2:3] * y_true[l][..., 3:4]
-
-        # Find ignore mask, iterate over each of batch.
-        ignore_mask = tf.TensorArray(K.dtype(y_true[0]), size=1, dynamic_size=True)
-        object_mask_bool = K.cast(object_mask, 'bool')
-
-        def loop_body (b, ignore_mask):
-            true_box = tf.boolean_mask(y_true[l][b, ..., 0:4], object_mask_bool[b, ..., 0])
-            iou = box_iou(pred_box[b], true_box)
-            best_iou = K.max(iou, axis=-1)
-            ignore_mask = ignore_mask.write(b, K.cast(best_iou < ignore_thresh, K.dtype(true_box)))
-            return b + 1, ignore_mask
-
-        _, ignore_mask = K.control_flow_ops.while_loop(lambda b, *args: b < m, loop_body, [0, ignore_mask])
-        ignore_mask = ignore_mask.stack()
-        ignore_mask = K.expand_dims(ignore_mask, -1)
-
-        # K.binary_crossentropy is helpful to avoid exp overflow.
-        xy_loss = object_mask * box_loss_scale * K.binary_crossentropy(raw_true_xy, raw_pred[..., 0:2],
-                                                                       from_logits=True)
-        wh_loss = object_mask * box_loss_scale * 0.5 * K.square(raw_true_wh - raw_pred[..., 2:4])
-        confidence_loss = object_mask * K.binary_crossentropy(object_mask, raw_pred[..., 4:5], from_logits=True) + \
-                          (1 - object_mask) * K.binary_crossentropy(object_mask, raw_pred[..., 4:5],
-                                                                    from_logits=True) * ignore_mask
-        class_loss = object_mask * K.binary_crossentropy(true_class_probs, raw_pred[..., 5:], from_logits=True)
-
-        xy_loss = K.sum(xy_loss) / mf
-        wh_loss = K.sum(wh_loss) / mf
-        confidence_loss = K.sum(confidence_loss) / mf
-        class_loss = K.sum(class_loss) / mf
-        loss += xy_loss + wh_loss + confidence_loss + class_loss
-        if print_loss:
-            loss = tf.Print(loss, [loss, xy_loss, wh_loss, confidence_loss, class_loss, K.sum(ignore_mask)],
-                            message='loss: ')
-    return loss
-"""
-
 class YOLO(object):
     _defaults = {
         "model_path": 'model_data/logs/test_model.h5',
@@ -692,15 +616,15 @@ class YOLO(object):
                 y1 = int(y_middle)
                 print("Touching at coordinate (%d,%d)"%(x1,y1))
                 touch((x1, y1))      #touch(v, times=1, **kwargs)
-                sleep(6)
+                sleep(3)
 
             elif predicted_class == "swipe_button":
                 print("Swipe button detected!")
                 x2 = int(x_middle)
                 y2 = int(y_middle)
                 print("Touching at coordinate (%d,%d)" % (x2, y2))
-                swipe((int(left),y_middle),(int(right),y_middle))      #swipe(v1, v2=None, vector=None, **kwargs) eg.swipe((100, 100), (200, 200))
-                sleep(6)
+                touch((x2, y2)) #swipe((int(left),y_middle),(int(right),y_middle))      #swipe(v1, v2=None, vector=None, **kwargs) eg.swipe((100, 100), (200, 200))
+                sleep(3)
 
             elif predicted_class == "swipe_bar":
                 print("Swipe bar detected!")
@@ -710,7 +634,7 @@ class YOLO(object):
                 y3_end = int(bottom)
                 print("Swiping from (%d,%d) to (%d,%d)" % (x3_start, y3_start, x3_end, y3_end))
                 swipe((x3_start, y3_start),(x3_end, y3_end))   #using coordinates      #swipe(v1, v2=None, vector=None, **kwargs)
-                sleep(6)
+                sleep(3)
 
             elif predicted_class == "bar":
                 print("Bar detected!")
